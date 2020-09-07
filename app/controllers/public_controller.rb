6c0193e552df76e08b077be38508d4acac7b4676
Yasir Saeed <55569291+yasir-saeed@users.noreply.github.com>
2020-09-07 19:45:32 -0400

    Update public_controller.rb

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,7 +2,7 @@ class PublicController < ApplicationController
 	layout 'public'
 
 def search
-    @public = Image.where("title LIKE ?", "%" + params[:q] + "%")
+    @public = Image.where("title LIKE ?", "%" + params[:q] + "%" + "COLLATE utf8_general_ci")
   end
 
 def about
